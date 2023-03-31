f5b6ea0de08334b3cae63fa86d300bb9fab1b469
WNJXYK <wnjxyk@gmail.com>
2023-04-01 03:13:25 +0800

    [MNT] Rewrite admin

--- api/admin.py
+++ api/admin.py
@@ -1,7 +1,6 @@
 from flask import Blueprint, g, jsonify, request
 from config import C
-from lib.user import get_all_user_info
-
+if C.database_type == "sqlite": import lib.sql_user as user_db
 
 admin_api = Blueprint("Admin-API", __name__)
 
@@ -20,5 +19,5 @@ def get_user_list():
         result = {"code": 2, "msg": "Permission denied."}
     else:
         result = {"code": 0, "msg": "Get user list success."}
-        result["data"] = get_all_user_info(columns=["username", "email"])
+        result["data"] = user_db.get_all_user_info(columns=["username", "email"])
     return jsonify(result)
