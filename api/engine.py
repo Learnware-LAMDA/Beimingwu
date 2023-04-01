09487a2d8ff489bd10400c121bf7f4cc20c15aa4
WNJXYK <wnjxyk@gmail.com>
2023-04-01 22:21:51 +0800

    [ENH] Add engine functions

--- api/engine.py
+++ api/engine.py
@@ -1,14 +1,33 @@
 from flask import Blueprint, jsonify, request
+from flask import send_from_directory, send_file, make_response
+from .auth import login_required
 from config import C
+import os
 
 engine_api = Blueprint("Engine-API", __name__)
-
 
 @engine_api.route("/get_semantic_specification", methods=["GET"])
 def get_semantic_specification():
     result = {
         "code": 0, 
         "msg": "Ok",
-        "data": C.engine.get_property_list(),
+        # "data": C.engine.get_property_list(),
     }
     return jsonify(result)
+
+@engine_api.route("/search_learnware", methods=["GET"])
+def search_learnware():
+    result = {
+        "code": 0, 
+        "msg": "Ok",
+        # "data": C.engine.get_property_list(),
+    }
+    return jsonify(result)
+
+@engine_api.route("/download_learnware", methods=["GET"])
+@login_required
+def download_learnware():
+    learnware_name = "file.zip"
+    learnware_path = os.path.join(C.root_path, "files")
+    response = make_response(send_from_directory(learnware_path, learnware_name, as_attachment=True))
+    return response
