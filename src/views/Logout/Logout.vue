eaf908b858ad2f9dc95bea850d365e7f24a5c6aa
AnnyTerfect <1247470246@qq.com>
2023-04-02 11:33:13 +0800

    [ENH] Add logout view

---
+++ src/views/Logout/Logout.vue
@@ -0,0 +1,18 @@
+<script setup>
+import { onMounted } from 'vue'
+import { useStore } from 'vuex'
+import { useRouter } from 'vue-router'
+
+const store = useStore()
+
+const router = useRouter()
+
+onMounted(() => {
+    store.commit('setLoggedIn', false)
+    router.push('/')
+})
+</script>
+
+<template>
+    <div></div>
+</template>
