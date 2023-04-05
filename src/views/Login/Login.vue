e4619fabda990d994a339d199d1ecdb17bad49cb
AnnyTerfect <1247470246@qq.com>
2023-04-05 15:01:58 +0800

    [ENH] Finished login view

--- src/views/Login/Login.vue
+++ src/views/Login/Login.vue
@@ -1,8 +1,9 @@
 <script setup>
-import { useDisplay } from 'vuetify'
-import { computed } from 'vue'
+import { ref, computed } from 'vue'
 import { useStore } from 'vuex'
+import { useDisplay } from 'vuetify'
 import { useRouter } from 'vue-router'
+import { useField, useForm } from 'vee-validate'
 
 const display = useDisplay()
 
@@ -18,34 +19,101 @@ const elevationClass = computed(() => {
   }
 })
 
-function login() {
-    store.commit('setLoggedIn', true)
-    router.push('/')
-}
+const { handleSubmit, meta } = useForm({
+  initialValues: {
+    email: 'a@a.a',
+    password: 'aaaaaaaa',
+  },
+  validationSchema: {
+    email(value) {
+      if (/^[a-z.-]+@[a-z.-]+\.[a-z]+$/i.test(value)) return true
+
+      return 'Must be a valid e-mail.'
+    },
+    password(value) {
+      if (value?.length >= 8) return true
+
+      return 'Password needs to be at least 8 characters.'
+    },
+  },
+})
+
+
+const email = useField('email')
+const password = useField('password')
+
+const showPassword = ref(false)
+const showError = ref(false)
+const errorMsg = ref('')
+const success = ref(false)
+
+const valid = computed(() => {
+  return meta.value.valid
+})
+
+const login = handleSubmit(values => {
+  const data = {
+    email: values.email,
+    password: values.password,
+  }
+
+  fetch('/api/auth/login', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(data)
+  })
+    .then((res) => res.json())
+    .then((res) => {
+      switch (res.code) {
+        case 0: {
+          success.value = true
+          setTimeout(() => {
+            store.commit('setLoggedIn', true)
+            router.push('/')
+          }, 1000)
+          return
+        }
+        default: {
+          showError.value = true
+          errorMsg.value = res.msg
+          setTimeout(() => showError.value = false, 3000)
+        }
+      }
+    })
+})
 </script>
 
 <template>
-    <div class="flex flex-row justify-center items-center fill-height p-2 md:text-md sm:text-sm text-xs bg-gray-100">
-        <v-card flat class="mx-auto w-1/1 p-4" :class="elevationClass" max-width="500">
-            <v-card-title>
-                <h1 class="text-lg-h4 text-h5 m-2">Login</h1>
-            </v-card-title>
-            <v-card-text>
-                <v-form ref="form" v-model="valid">
-                    <v-text-field v-model="email" label="E-mail"></v-text-field>
-                    <v-text-field v-model="password" :append-icon="showPassword ? 'mdi-eye' : 'mdi-eye-off'"
-                        :rules="passwordRules" :type="showPassword ? 'text' : 'password'" label="Password" required
-                        @click:append="showPassword = !showPassword"></v-text-field>
-                </v-form>
-            </v-card-text>
-            <v-card-actions>
-                <v-btn block class="bg-primary py-5" color="white" @click="login">Login</v-btn>
-            </v-card-actions>
-            <v-card-actions>
-                Don't have an account?
-                <v-spacer></v-spacer>
-                <v-btn variant="text" color="primary" @click="register">Register first</v-btn>
-            </v-card-actions>
-        </v-card>
-    </div>
+  <div class="flex flex-row justify-center items-center fill-height p-2 md:text-md sm:text-sm text-xs bg-gray-100">
+    <v-card flat class="mx-auto w-1/1 p-4" :class="elevationClass" max-width="500">
+      <v-card-title>
+        <h1 class="text-lg-h4 text-h5 m-2">Login</h1>
+      </v-card-title>
+      <v-card-text>
+        <v-form ref="form">
+          <v-text-field v-model="email.value.value" label="E-mail"
+            :error-messages="email.errorMessage.value"></v-text-field>
+          <v-text-field v-model="password.value.value" :append-icon="showPassword ? 'mdi-eye' : 'mdi-eye-off'"
+            :type="showPassword ? 'text' : 'password'" label="Password" :error-messages="password.errorMessage.value"
+            @click:append="showPassword = !showPassword"></v-text-field>
+        </v-form>
+      </v-card-text>
+      <v-card-actions v-if="success">
+        <v-alert closable text="Login successfully" type="success" />
+      </v-card-actions>
+      <v-card-actions v-if="showError">
+        <v-alert closable :text="errorMsg" type="error" />
+      </v-card-actions>
+      <v-card-actions>
+        <v-btn block class="bg-primary py-5" color="white" @click="login" :disabled="!valid">Login</v-btn>
+      </v-card-actions>
+      <v-card-actions>
+        Don't have an account?
+        <v-spacer></v-spacer>
+        <v-btn variant="text" color="primary" @click="router.push('/register')">Register first</v-btn>
+      </v-card-actions>
+    </v-card>
+  </div>
 </template>
