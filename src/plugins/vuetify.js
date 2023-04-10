356360bb53b5d0f9ec6a2f59cb34feaf8fb442f3
AnnyTerfect <1247470246@qq.com>
2023-04-10 14:36:38 +0800

    [ENH] Edit theme style

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -4,7 +4,30 @@ import { createVuetify } from 'vuetify'
 import * as components from 'vuetify/components'
 import * as directives from 'vuetify/directives'
 
+const myCustomLightTheme = {
+    dark: false,
+    colors: {
+        background: '#FFFFFF',
+        surface: '#FFFFFF',
+        primary: '#6200EE',
+        'primary-darken-1': '#3700B3',
+        secondary: '#03DAC6',
+        'secondary-darken-1': '#018786',
+        error: '#B00020',
+        info: '#2196F3',
+        success: '#4CAF50',
+        warning: '#FB8C00',
+    }
+}
+
+
 export default createVuetify({
+    theme: {
+        defaultTheme: 'myCustomLightTheme',
+        themes: {
+            myCustomLightTheme,
+        }
+    },
     components,
     directives,
     icons: {
