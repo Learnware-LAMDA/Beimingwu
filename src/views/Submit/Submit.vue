e52e0ca9ef55461e21fcd2bc1cc87bacabe040f4
Qincheng Zheng <zhengqc@lamda.nju.edu.cn>
2023-04-19 16:24:33 +0800

    [MNT] Componentize submiting dialog

--- src/views/Submit/Submit.vue
+++ src/views/Submit/Submit.vue
@@ -7,6 +7,7 @@ import { useField, useForm } from 'vee-validate'
 import VStepperTitle from '@/components/Public/VStepperTitle.vue'
 import FileUpload from '@/components/Specification/FileUpload.vue'
 import SpecTag from '@/components/Specification/SpecTag.vue'
+import SubmitingDialog from './SubmitingDialog.vue'
 
 const route = useRoute()
 const router = useRouter()
@@ -152,28 +153,40 @@ const submit = handleSubmit((values) => {
   success.value = false
   showError.value = false
 
-  fetch('/api/engine/get_semantic_specification')
-    .then((res) => res.json())
-    .then((res) => {
-      const semanticSpec = res.data.semantic_specification
-      semanticSpec.Name.Values = values.name
-      semanticSpec.Data.Values = [values.dataType]
-      semanticSpec.Task.Values = [values.taskType]
-      semanticSpec.Device.Values = values.deviceType
-      semanticSpec.Scenario.Values = values.tagList
-      semanticSpec.Description.Values = values.description
+  const semanticSpec = {
+    "Data": {
+      "Values": [dataType.value.value],
+      "Type": "Class"
+    },
+    "Task": {
+      "Values": [taskType.value.value],
+      "Type": "Class"
+    },
+    "Device": {
+      "Values": deviceType.value.value,
+      "Type": "Tag"
+    },
+    "Scenario": {
+      "Values": tagList.value.value,
+      "Type": "Tag"
+    },
+    "Description": {
+      "Values": description.value.value,
+      "Type": "Description"
+    },
+    "Name": {
+      "Values": name.value.value,
+      "Type": "Name"
+    }
+  }
+  const fd = new FormData()
+  fd.append('learnware_file', files.value.value[0])
+  fd.append('semantic_specification', JSON.stringify(semanticSpec))
 
-      const fd = new FormData()
-      fd.append('learnware_file', values.files[0])
-      fd.append('semantic_specification', JSON.stringify(semanticSpec))
-      return fd
-    })
-    .then((fd) => {
-      return fetch('/api/user/add_learnware', {
-        method: 'POST',
-        body: fd,
-      })
-    })  
+  fetch('/api/user/add_learnware', {
+    method: 'POST',
+    body: fd,
+  })
     .then((res) => {
       if (res.status === 200) {
         return res
@@ -348,48 +361,7 @@ onMounted(() => {
         </v-card-actions>
       </div>
     </v-card>
-    <v-dialog v-model="submiting" persistent class="max-w-600px w-1/1">
-      <v-card class="p-2">
-        <v-card-title class="my-4 text-center">
-          <svg class="m-auto w-1/3" version="1.1" id="L1" xmlns="http://www.w3.org/2000/svg"
-            xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 100 100"
-            enable-background="new 0 0 100 100" xml:space="preserve">
-            <circle fill="none" stroke-width="6" stroke-miterlimit="15" stroke-dasharray="14.2472,14.2472" cx="50" cy="50"
-              r="47" style="stroke: rgb(var(--v-theme-primary))">
-              <animateTransform attributeName="transform" attributeType="XML" type="rotate" dur="5s" from="0 50 50"
-                to="360 50 50" repeatCount="indefinite" />
-            </circle>
-            <circle fill="none" stroke-width="1" stroke-miterlimit="10" stroke-dasharray="10,10" cx="50" cy="50" r="39"
-              style="stroke: rgb(var(--v-theme-primary))">
-              <animateTransform attributeName="transform" attributeType="XML" type="rotate" dur="5s" from="0 50 50"
-                to="-360 50 50" repeatCount="indefinite" />
-            </circle>
-            <g style="fill: rgb(var(--v-theme-primary))">
-              <rect x="30" y="35" width="5" height="30">
-                <animateTransform attributeName="transform" dur="1s" type="translate" values="0 5 ; 0 -5; 0 5"
-                  repeatCount="indefinite" begin="0.1" />
-              </rect>
-              <rect x="40" y="35" width="5" height="30">
-                <animateTransform attributeName="transform" dur="1s" type="translate" values="0 5 ; 0 -5; 0 5"
-                  repeatCount="indefinite" begin="0.2" />
-              </rect>
-              <rect x="50" y="35" width="5" height="30">
-                <animateTransform attributeName="transform" dur="1s" type="translate" values="0 5 ; 0 -5; 0 5"
-                  repeatCount="indefinite" begin="0.3" />
-              </rect>
-              <rect x="60" y="35" width="5" height="30">
-                <animateTransform attributeName="transform" dur="1s" type="translate" values="0 5 ; 0 -5; 0 5"
-                  repeatCount="indefinite" begin="0.4" />
-              </rect>
-              <rect x="70" y="35" width="5" height="30">
-                <animateTransform attributeName="transform" dur="1s" type="translate" values="0 5 ; 0 -5; 0 5"
-                  repeatCount="indefinite" begin="0.5" />
-              </rect>
-            </g>
-          </svg>
-        </v-card-title>
-      </v-card>
-    </v-dialog>
+    <submiting-dialog v-if="submiting" />
   </v-container>
 </template>
 
