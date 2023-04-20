35f217ef85dd2e426afcbd0d00eca00631825c6b
AnnyTerfect <1247470246@qq.com>
2023-04-20 12:41:19 +0800

    [MNT] Remove bigger animation

--- src/components/Home/Slogan.vue
+++ src/components/Home/Slogan.vue
@@ -1,64 +1,13 @@
 <script setup>
 import BigTitle from '@/components/Public/BigTitle.vue'
-import { ref, onMounted, nextTick, onUnmounted } from 'vue'
+import { ref, onMounted } from 'vue'
 import { useRouter } from 'vue-router'
 
 const router = useRouter()
-
-const ratio = ref(0)
-
-const scroller = ref(null)
-const placeholder = ref(null)
-
-function handleScroll() {
-  ratio.value = window.scrollY / window.innerHeight * 2
-  document.documentElement.style.setProperty('--scale', `${window.scrollY / window.innerHeight * 3 + 1}`)
-}
-
-onMounted(() => {
-  nextTick(() => {
-    placeholder.value.style.height = `${scroller.value.clientHeight + window.innerHeight / 2}px`
-
-    window.addEventListener('scroll', handleScroll)
-  })
-})
-
-onUnmounted(() => {
-  document.documentElement.style.setProperty('--scale', '1')
-  window.removeEventListener('scroll', handleScroll)
-})
 </script>
 
 <template>
-  <div class="d-sm-block d-none">
-    <div ref="scroller" class="left-0 right-0 z-index-100 overflow-hidden" :class="ratio <= 1 ? ['fixed'] : null"
-      :style="ratio > 1 ? { marginTop: `50vh` } : null">
-      <div class="xl:py-35
-                        lg:py-25
-                        <lg:py-10
-                        text-center bg-primary text-white">
-        <big-title>
-          <h1>Small models do</h1>
-          <h1 class="py-5 transform" style="position: relative; z-index: 1000; transform: scale(var(--scale))"><span
-              class="text-1.7em">Big</span></h1>
-        </big-title>
-        <div class="mx-10 white-space-pre-wrap
-                                lg:text-2xl
-                                sm:text-lg">
-          <div class="mt-6">
-            Learnware paradigm systematically reuses numerous various models to do things that may even be beyond their original purposes, <br>and enables users to easily identify and reuse helpful models, not need to build their models from scratch.
-          </div>
-        </div>
-
-        <div class="flex justify-center mt-12">
-          <v-btn class="mx-3" size="large" @click="router.push('/search')">Try it out</v-btn>
-          <v-btn class="mx-3" variant="outlined" size="large" @click="router.push('/submit')">Be a provider</v-btn>
-        </div>
-      </div>
-    </div>
-    <div ref="placeholder" v-show="ratio <= 1"></div>
-  </div>
-  <div class="d-sm-none d-block">
+  <div>
     <div class="xl:py-30
                         lg:py-25
                         <lg:py-10
@@ -70,8 +19,8 @@ onUnmounted(() => {
       <div class="mx-9 white-space-pre-wrap
                                 lg:text-2xl
                                 sm:text-lg">
-        <div class="mt-6 text-0.9em leading-5">
-          Learnware paradigm systematically reuses numerous various models to do things that may even be beyond their original purposes, and enables users to easily identify and reuse helpful models, not need to build their models from scratch.
+        <div class="mt-6 <sm:(text-0.9em leading-5)">
+          Learnware paradigm systematically reuses numerous various models to do things that may even be beyond their original purposes, <br>and enables users to easily identify and reuse helpful models, not need to build their models from scratch.
         </div>
       </div>
 
