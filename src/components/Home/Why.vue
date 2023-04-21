2f9aaab32062c7ae53e85694220f6860a08d1bb1
AnnyTerfect <1247470246@qq.com>
2023-04-21 13:27:13 +0800

    [MNT] Optimize the styles

--- src/components/Home/Why.vue
+++ src/components/Home/Why.vue
@@ -36,7 +36,7 @@ const reasons = ref([
 </script>
 
 <template>
-  <div class="mx-auto md:py-30 <md:py-20 md:px-10 max-w-1200px w-1/1">
+  <div class="mx-auto md:pb-30 <md:pb-20 md:px-10 max-w-1200px w-1/1">
     <div class="<md:px-5">
       <div class="xl:(text-5xl my-10) lg:(text-4xl my-7) md:(text-3xl my-5) <md:(text-4xl my-5)">
         Why do you need learnware?
