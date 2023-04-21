03ee5d9e34103eb11ca4e946fe5d3a5c693d39c2
AnnyTerfect <1247470246@qq.com>
2023-04-21 12:53:25 +0800

    [MNT] Optimize the style

--- src/components/Home/Slogan.vue
+++ src/components/Home/Slogan.vue
@@ -7,16 +7,16 @@ const router = useRouter()
 </script>
 
 <template>
-  <div class="xl:py-30 lg:py-25 <lg:py-10 text-center text-white" style="background: #6200EE; background: -webkit-linear-gradient(to right, #673AB7, #6200EE); background: linear-gradient(to right, #673AB7, #6200EE);">
+  <div class="xl:py-30 lg:py-30 md:py-20 <md:py-10 text-center text-white" style="background: #6200EE; background: -webkit-linear-gradient(to right, #673AB7, #6200EE); background: linear-gradient(to right, #673AB7, #6200EE);">
     <big-title>
       <h1>Small models do</h1>
       <h1 class="py-5 transform"><span class="text-1.7em">Big</span></h1>
     </big-title>
-    <div class="mx-9 white-space-pre-wrap
-                              lg:text-2xl
-                              sm:text-lg">
-      <div class="mt-6 <sm:(text-0.9em leading-5)">
-        Learnware paradigm systematically reuses numerous various models to do things that may even be beyond their original purposes, <br>and enables users to easily identify and reuse helpful models, not need to build their models from scratch.
+    <div class="md:mx-20 sm:mx-15 <sm:mx-9">
+      <div class="mx-auto max-w-1200px white-space-pre-wrap lg:text-xl md:(text-1rem leading-6) sm:(text-0.9rem leading-5)">
+        <div class="mt-6 <sm:(text-0.9em leading-5)">
+          Learnware paradigm systematically reuses numerous various models to do things that may even be beyond their original purposes, and enables users to easily identify and reuse helpful models, not need to build their models from scratch.
+        </div>
       </div>
     </div>
 
