dce2eb138d10954d4a80ff272566a0a3fe7742aa
AnnyTerfect <1247470246@qq.com>
2023-04-21 13:18:06 +0800

    [MNT] Optimize the style

--- src/components/Home/How.vue
+++ src/components/Home/How.vue
@@ -1,28 +1,22 @@
 <script setup>
-import { computed } from 'vue';
-import { useDisplay } from 'vuetify'
 import process from '/process.png'
-
-const display = useDisplay()
-
-const greaterThanMd = computed(() => ['lg', 'xl'].includes(display.name.value))
-
-const classGreaterThanMd = ['elevation-10']
 </script>
 
 <template>
-  <div class="max-w-1200px w-1/1 m-auto lg:py-20 md:py-13 sm:py-10 py-10">
-    <div class="xl:(text-6xl mb-16)
-                  lg:(text-5xl mb-13)
-                  md:(text-4xl mb-10)
-                  <md:(text-3xl mb-10)
-                  text-3xl
-                  text-center">
-      How learnware works?
-    </div>
-
-    <v-responsive :class="greaterThanMd ? classGreaterThanMd : null" :aspect-ratio="96/55">
-      <v-img contain width="100%" :src="process" />
-    </v-responsive>
+  <div class="mx-auto md:py-30 <md:py-20 md:px-10 max-w-1200px w-1/1">
+    <div class="<md:px-5">
+      <div class="xl:(text-5xl my-10) lg:(text-4xl my-7) md:(text-3xl my-5) <md:(text-4xl my-5)">
+        How does learnware works?
+      </div>
+      <p class="text-gray-500">
+        There are three important entities: developers, users, and the market. The developers are usually machine learning
+        experts who produce and want to share/sell their well-performed trained machine learning models. The users need
+        machine learning services but usually have only limited data and lack machine learning knowledge and skills. The
+        learnware market accepts/buys well-performed trained models from developers, accommodates them in the market, and
+        provides/sells services to users via identifying and reusing learnwares to help users tackle their present tasks
+      </p>
+      <v-responsive class="mt-10 sm:shadow-md" :aspect-ratio="96 / 55">
+        <v-img contain width="100%" :src="process" />
+      </v-responsive>
   </div>
-</template>
+</div></template>
