64e467a6e37560ff027acc53f513523aea7f7eea
Qincheng Zheng <zhengqc@lamda.nju.edu.cn>
2023-04-26 11:16:47 +0800

    [MNT] Componentize UserRequirement

---
+++ src/components/Search/UserRequirement.vue
@@ -0,0 +1,90 @@
+<script setup>
+import { ref, computed, watch } from 'vue'
+import { useRoute } from 'vue-router'
+import DataType from '@/components/Specification/SpecTag/DataType.vue'
+import TaskType from '@/components/Specification/SpecTag/TaskType.vue'
+import LibraryType from '@/components/Specification/SpecTag/LibraryType.vue'
+import FileUpload from '@/components/Specification/FileUpload.vue'
+import TagList from '@/components/Specification/SpecTag/TagList.vue'
+
+const emits = defineEmits(['update:value'])
+
+const props = defineProps({
+  value: {
+    type: Object,
+    default: () => ({})
+  }
+})
+
+const route = useRoute()
+
+const search = ref(route.query.search || '')
+const dataType = ref(route.query.dataType || '')
+const taskType = ref(route.query.taskType || '')
+const libraryType = ref(route.query.libraryType || '')
+let _tagList
+try {
+  _taglist = JSON.parse(route.query.tagList)
+}
+catch {
+  _tagList = []
+}
+const tagList = ref(_tagList)
+
+const files = ref([])
+
+const requirement = computed(() => ({
+  name: search.value,
+  dataType: dataType.value,
+  taskType: taskType.value,
+  libraryType: libraryType.value,
+  tagList: tagList.value,
+  files: files.value
+}))
+
+watch(
+  () => requirement.value,
+  () => {
+    emits('update:value', requirement.value)
+  }
+)
+</script>
+
+<template>
+  <div class="flex flex-col w-1/1 md:max-w-460px sm:border-r-1">
+    <div class="filter">
+      <div class="my-3 text-h6">
+        <v-icon class="!mt-0 mr-3" icon="mdi-tag-text" color="black" size="small" />Choose semantic requirement
+      </div>
+
+      <div>
+        <div class="mt-7 mb-3 text-h6 !text-1rem">Search by name</div>
+        <v-text-field v-model="search" label="Learnware name" hide-details append-inner-icon="mdi-close"
+          @click:append-inner="search = ''" />
+      </div>
+
+      <data-type :cols="3" :md="2" :sm="2" :xs="2" v-model:value="dataType" />
+      <task-type :cols="2" :md="2" :sm="2" :xs="2" v-model:value="taskType" />
+      <library-type :cols="2" :md="2" :sm="2" :xs="2" v-model:value="libraryType" />
+      <tag-list class="bg-transparent !text-1rem" v-model:value="tagList" :cols="2" :md="2" :sm="2" :xs="2" />
+    </div>
+
+    <div class="p-5 pt-0 border-t-1 border-gray-300">
+      <div ref="anchorRef" class="mt-3 mb-5 w-1/1 text-h6 transition-all truncate">
+        <v-icon class="mr-3" icon="mdi-upload" color="black" size="small" />Upload statistical requirement
+      </div>
+
+      <file-upload v-model:files="files" :height="28" />
+    </div>
+  </div>
+</template>
+
+<style scoped lang="scss">
+.filter {
+  @apply p-2 w-1/1 md: (h-1/1 overflow-y-scroll) sm:px-5;
+
+  * {
+    @apply mt-2;
+  }
+}
+</style>
--- src/views/Search/Search.vue
+++ src/views/Search/Search.vue
@@ -1,35 +1,21 @@
 <script setup>
 import { ref, computed, watch, onMounted, nextTick, onActivated } from 'vue'
-import { useRoute } from 'vue-router'
 import { useDisplay } from 'vuetify'
-import DataType from '@/components/Specification/SpecTag/DataType.vue'
-import TaskType from '@/components/Specification/SpecTag/TaskType.vue'
-import LibraryType from '@/components/Specification/SpecTag/LibraryType.vue'
-import FileUpload from '@/components/Specification/FileUpload.vue'
-import TagList from '@/components/Specification/SpecTag/TagList.vue'
+import UserRequirement from '@/components/Search/UserRequirement.vue'
 import PageLearnwareList from '@/components/Learnware/PageLearnwareList.vue'
 import MultiRecommendedLearnwareList from '@/components/Learnware/MultiRecommendedLearnwareList.vue'
 
-const route = useRoute()
-
 const display = useDisplay()
 
-const search = ref(route.query.search || '')
-const dataType = ref(route.query.dataType || '')
-const taskType = ref(route.query.taskType || '')
-const libraryType = ref(route.query.libraryType || '')
-let _tagList
-try {
-  _taglist = JSON.parse(route.query.tagList)
-}
-catch {
-  _tagList = []
-}
-const tagList = ref(_tagList)
+const filters = ref({
+  name: '',
+  dataType: '',
+  taskType: '',
+  libraryType: '',
+  tagList: [],
+  files: [],
+})
 
-const files = ref([])
-
-const multiRecommendedLearnwareSize = ref(4)
 const multiRecommendedLearnwareItems = ref([])
 const multiRecommendedMatchScore = ref(null)
 const singleRecommendedLearnwarePage = ref(1)
@@ -53,15 +39,6 @@ const showMultiRecommended = computed(() => {
 const multiRecommendedTips = ref(true)
 const singleRecommendedTips = ref(true)
 
-const filters = computed(() => ({
-  name: search.value,
-  dataType: dataType.value,
-  taskType: taskType.value,
-  libraryType: libraryType.value,
-  tagList: tagList.value,
-  files: files.value
-}))
-
 function pageChange(newPage) {
   singleRecommendedLearnwarePage.value = newPage
 }
@@ -74,16 +51,16 @@ function fetchByFilterAndPage(filters, page) {
     .then((res) => res.json())
     .then((res) => {
       const semanticSpec = res.data.semantic_specification
-      semanticSpec.Name.Values = filters.name
+      semanticSpec.Name.Values = filters.name ? filters.name : ''
       semanticSpec.Data.Values = filters.dataType ? [filters.dataType] : []
       semanticSpec.Task.Values = filters.taskType ? [filters.taskType] : []
       semanticSpec.Library.Values = filters.libraryType ? [filters.libraryType] : []
-      semanticSpec.Scenario.Values = filters.tagList
+      semanticSpec.Scenario.Values = filters.tagList ? filters.tagList : []
       semanticSpec.Description.Values = ''
 
       const fd = new FormData()
       fd.append('semantic_specification', JSON.stringify(semanticSpec))
-      fd.append('statistical_specification', files.value.length > 0 ? files.value[0] : null)
+      fd.append('statistical_specification', filters.files?.length > 0 ? filters.files[0] : null)
       fd.append('limit', singleRecommendedLearnwarePageSize.value)
       fd.append('page', page)
       return fd
@@ -129,7 +106,7 @@ function fetchByFilterAndPage(filters, page) {
             taskType: item.semantic_specification.Task.Values[0],
             libraryType: item.semantic_specification.Library.Values[0],
             tagList: item.semantic_specification.Scenario.Values,
-            matchScore: files.value.length > 0 ? Math.floor(item.matching * 100) : null
+            matchScore: filters.files?.length > 0 ? Math.floor(item.matching * 100) : null
           }))
           singleRecommendedLearnwarePageNum.value = res.data.total_pages
           return
@@ -152,7 +129,6 @@ function fetchByFilterAndPage(filters, page) {
 watch(
   () => filters.value,
   () => {
-    // saveQuery()
     singleRecommendedLearnwarePage.value = 1
   },
   { deep: true }
@@ -196,8 +172,6 @@ onMounted(() => {
     contentRef.value.addEventListener('scroll', () => {
       scrollTop.value = contentRef.value.scrollTop
     })
-
-    // loadQuery()
   })
 })
 </script>
@@ -209,29 +183,9 @@ onMounted(() => {
         <v-alert class="w-1/1 max-w-900px mx-auto" closable :text="errorMsg" type="error" @click:close="showError = false" />
       </v-card-actions>
     </v-scroll-y-transition>
-    <div class="flex flex-col w-1/1 md:max-w-460px sm:border-r-1">
-      <div class="filter">
-        <div class="my-3 text-h6">
-          <v-icon class="!mt-0 mr-3" icon="mdi-tag-text" color="black" size="small" />Choose semantic requirement
-        </div>
-        <div>
-          <div class="mt-7 mb-3 text-h6 !text-1rem">Search by name</div>
-          <v-text-field v-model="search" label="Learnware name" hide-details append-inner-icon="mdi-close"
-            @click:append-inner="search = ''" />
-        </div>
-        <data-type :cols="3" :md="2" :sm="2" :xs="2" v-model:value="dataType" />
-        <task-type :cols="2" :md="2" :sm="2" :xs="2" v-model:value="taskType" />
-        <library-type :cols="2" :md="2" :sm="2" :xs="2" v-model:value="libraryType" />
-        <tag-list class="bg-transparent !text-1rem" v-model:value="tagList" :cols="2" :md="2" :sm="2" :xs="2" />
-      </div>
-      <div class="p-5 pt-0 border-t-1 border-gray-300">
-        <div ref="anchorRef" class="mt-3 mb-5 w-1/1 text-h6 transition-all truncate">
-          <v-icon class="mr-3" icon="mdi-upload" color="black" size="small" />Upload statistical requirement
-        </div>
 
-        <file-upload v-model:files="files" :height="28" />
-      </div>
-    </div>
+    <user-requirement v-model:value="filters" />
+    
     <div ref="contentRef" class="content">
       <v-card v-if="showMultiRecommended" flat class="sm:m-2 mt-4 bg-transparent">
         <v-card-title v-if="!multiRecommendedTips">Recommended multiple learnwares</v-card-title>
@@ -270,18 +224,6 @@ onMounted(() => {
 .search-container {
   @apply md: (fixed flex) mx-auto w-1/1;
   height: calc(100% - var(--v-layout-top));
-
-  .filter {
-    @apply p-2 w-1/1 md: (h-1/1 overflow-y-scroll) sm:px-5;
-
-    * {
-      @apply mt-2;
-    }
-  }
-
-  .filter.hide {
-    @apply h-0;
-  }
 
   .content {
     @apply w-1/1 overflow-y-scroll;
