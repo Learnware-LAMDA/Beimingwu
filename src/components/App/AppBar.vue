66a0bbd329ba7a378f8c2d4e3d41bf482f4797df
AnnyTerfect <1247470246@qq.com>
2023-06-23 11:41:13 +0800

    feat: btn -> router-link in appbar

--- src/components/App/AppBar.vue
+++ src/components/App/AppBar.vue
@@ -52,17 +52,16 @@ const filteredRoutes = computed(() =>
       <v-spacer></v-spacer>
 
       <v-toolbar-items v-if="!['xs', 'sm'].includes(display.name.value)">
-        <v-btn
-          v-for="route in filteredRoutes"
-          :key="route.name"
-          class="mr-2 text-body-2 rounded"
-          :variant="route.meta.variant"
-          :class="route.meta.class"
-          @click="() => router.push(route.path)"
-        >
-          <v-icon class="mr-1" :icon="route.meta.icon"></v-icon>
-          {{ route.meta.name || route.name }}
-        </v-btn>
+        <router-link v-for="route in filteredRoutes" :key="route.name" :to="route.path">
+          <v-btn
+            class="mr-2 !h-1/1 text-body-2 rounded"
+            :variant="route.meta.variant"
+            :class="route.meta.class"
+          >
+            <v-icon class="mr-1" :icon="route.meta.icon"></v-icon>
+            {{ route.meta.name || route.name }}
+          </v-btn>
+        </router-link>
       </v-toolbar-items>
     </div>
   </v-app-bar>
