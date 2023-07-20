2b660f4056fda24d89d44af9f8b57b8a9e6d94ef
Qincheng Zheng <zhengqc@lamda.nju.edu.cn>
2023-07-20 10:19:53 +0800

    style: move tips out of container

--- src/components/Specification/DescriptionInput.vue
+++ src/components/Specification/DescriptionInput.vue
@@ -74,45 +74,45 @@ watch(
 </script>
 
 <template>
-  <v-container class="max-h-[600px] overflow-y-scroll">
-    <v-row class="mt-3">
-      <v-alert type="info" color="primary" closable>
-        Fill in the description for each {{ name }}
-        <span class="d-none d-sm-inline">on the left</span>
-        or paste a JSON object<span class="d-none d-sm-inline"> on the right</span>. Clarifying the
-        description for each {{ name }} will help your learnware to be available for tasks with
-        hetergenous {{ name }} space.
-      </v-alert>
-    </v-row>
-    <v-row>
-      <v-col cols="12" md="6">
-        <v-text-field
-          v-for="(_, idx) in descriptionArray"
-          :key="idx"
-          v-model="descriptionArray[idx]"
-          :label="`Description: ${name} ${idx}`"
-          class="mb-1"
-          hide-details
-          prepend-icon="mdi-plus"
-          append-icon="mdi-delete"
-          @click:prepend="() => descriptionArray.splice(idx, 0, null)"
-          @click:append="() => (descriptionArray = descriptionArray.filter((_, i) => i !== idx))"
-        />
-        <v-btn block flat class="mt-1" @click="descriptionArray = [...descriptionArray, null]">
-          <v-icon size="large" color="#555">mdi-plus</v-icon>
-        </v-btn>
-      </v-col>
-      <v-col cols="12" md="6">
-        <v-textarea
-          v-model="descriptionString"
-          auto-grow
-          class="flex flex-col"
-          :label="`${name.slice(0, 1).toUpperCase()}${name.slice(1)} Description`"
-          :error-messages="errorMessages"
-        />
-      </v-col>
-    </v-row>
-  </v-container>
+  <div class="mt-3">
+    <v-alert type="info" color="primary" closable>
+      Fill in the description for each {{ name }}
+      <span class="d-none d-sm-inline">on the left</span>
+      or paste a JSON object<span class="d-none d-sm-inline"> on the right</span>. Clarifying the
+      description for each {{ name }} will help your learnware to be available for tasks with
+      hetergenous {{ name }} space.
+    </v-alert>
+    <v-container class="mt-3 max-h-[600px] overflow-y-scroll">
+      <v-row>
+        <v-col cols="12" md="6">
+          <v-text-field
+            v-for="(_, idx) in descriptionArray"
+            :key="idx"
+            v-model="descriptionArray[idx]"
+            :label="`Description: ${name} ${idx}`"
+            class="mb-1"
+            hide-details
+            prepend-icon="mdi-plus"
+            append-icon="mdi-delete"
+            @click:prepend="() => descriptionArray.splice(idx, 0, null)"
+            @click:append="() => (descriptionArray = descriptionArray.filter((_, i) => i !== idx))"
+          />
+          <v-btn block flat class="mt-1" @click="descriptionArray = [...descriptionArray, null]">
+            <v-icon size="large" color="#555">mdi-plus</v-icon>
+          </v-btn>
+        </v-col>
+        <v-col cols="12" md="6">
+          <v-textarea
+            v-model="descriptionString"
+            auto-grow
+            class="flex flex-col"
+            :label="`${name.slice(0, 1).toUpperCase()}${name.slice(1)} Description`"
+            :error-messages="errorMessages"
+          />
+        </v-col>
+      </v-row>
+    </v-container>
+  </div>
 </template>
 
 <style scoped>
