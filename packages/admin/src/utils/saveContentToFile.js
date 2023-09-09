18258ed61a5956c51811215886c3d823f4a25383
AnnyTerfect <1247470246@qq.com>
2023-09-09 23:45:04 +0800

    feat: save content to file function

---
+++ packages/admin/src/utils/saveContentToFile.js
@@ -0,0 +1,11 @@
+export default function saveContentToFile(content, fileName) {
+  const blob = new Blob([content], { type: 'text/plain;charset=utf-8' });
+  const url = window.URL.createObjectURL(blob);
+  const link = document.createElement('a');
+  document.body.appendChild(link);
+  link.href = url;
+  link.download = fileName;
+  link.click();
+  document.body.removeChild(link);
+  window.URL.revokeObjectURL(url);
+}
