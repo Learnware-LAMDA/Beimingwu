b823cadfd281a20bb1137fd5adce6c85df4675b2
Qincheng Zheng <zhengqc@lamda.nju.edu.cn>
2023-10-13 17:45:58 +0800

    chore(frontend): add checker and eslint plugins

--- frontend/packages/main/vite.config.ts
+++ frontend/packages/main/vite.config.ts
@@ -2,11 +2,12 @@ import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import WindiCSS from "vite-plugin-windicss";
 import eslintPlugin from "vite-plugin-eslint";
+import checker from "vite-plugin-checker";
 import svgLoader from "vite-svg-loader";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), WindiCSS(), eslintPlugin(), svgLoader()],
+  plugins: [vue(), WindiCSS(), svgLoader(), eslintPlugin(), checker({ typescript: true })],
   base: "./",
   server: {
     proxy: {
