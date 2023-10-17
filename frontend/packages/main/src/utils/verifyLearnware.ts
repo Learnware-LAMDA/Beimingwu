f9e1d4ee3144ec7353f0843f1bf8581cabc6f809
AnnyTerfect <1247470246@qq.com>
2023-10-17 23:14:53 +0800

    fix(frontend): remove console.log

--- frontend/packages/main/src/utils/verifyLearnware.ts
+++ frontend/packages/main/src/utils/verifyLearnware.ts
@@ -111,7 +111,6 @@ function verifyLearnware(file: ArrayBuffer, t: ComposerTranslation): Promise<str
           if (!zipSpec) {
             return t("Submit.File.Error.FileNotFound", { filename: spec.file_name });
           }
-          console.log(zipSpec);
           return zipSpec
             .async("string")
             .then(JSON.parse)
