8fee0bce0fbd727c17ae7af48cd45a44dde7750e
AnnyTerfect <1247470246@qq.com>
2023-10-21 13:24:20 +0800

    chore(frontend): bundle types using tsup

---
+++ frontend/tsup.config.ts
@@ -0,0 +1,11 @@
+import { defineConfig } from "tsup";
+
+export default defineConfig({
+  format: ["esm", "cjs"],
+  target: "node18",
+  splitting: true,
+  dts: true,
+  clean: true,
+  shims: false,
+  external: [],
+});
