59f064b75cd86e3f8d42945859be54a7ecd18556
AnnyTerfect <1247470246@qq.com>
2023-11-08 19:20:08 +0800

    feat(frontend): addscroll animate component

---
+++ frontend/packages/main/src/components/App/ScrollAnimate.vue
@@ -0,0 +1,32 @@
+<script lang="ts" setup>
+import { ref, computed } from "vue";
+import { useElementBounding } from "@vueuse/core";
+
+const anchorElement = ref<HTMLElement>();
+const stickyElement = ref<HTMLElement>();
+const animateElement = ref<HTMLElement>();
+
+const { y } = useElementBounding(anchorElement);
+const { height: stickyHeight } = useElementBounding(stickyElement);
+const { height: animateHeight } = useElementBounding(animateElement);
+
+const progress = computed(() => {
+  return Math.max(0, Math.min(1, -y.value / (stickyHeight.value - animateHeight.value)));
+});
+</script>
+
+<template>
+  <div ref="stickyElement">
+    <div ref="anchorElement" />
+    <div
+      class="sticky"
+      :style="{
+        top: 'var(--v-layout-top)',
+      }"
+    >
+      <div ref="animateElement">
+        <slot :progress="progress" />
+      </div>
+    </div>
+  </div>
+</template>
