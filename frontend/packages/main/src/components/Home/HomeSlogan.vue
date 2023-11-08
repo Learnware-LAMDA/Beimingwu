08487627cb7c4e3bd89aef4e65688ff70de62d85
AnnyTerfect <1247470246@qq.com>
2023-11-08 19:57:06 +0800

    feat(frontend): add search demo animation

--- frontend/packages/main/src/components/Home/HomeSlogan.vue
+++ frontend/packages/main/src/components/Home/HomeSlogan.vue
@@ -1,10 +1,27 @@
 <script setup lang="ts">
+import { ref, onMounted, nextTick } from "vue";
 import { useRouter } from "vue-router";
 import { useI18n } from "vue-i18n";
 import BigTitle from "../Public/BigTitle.vue";
-import system from "../../../public/system.png";
+import system from "../../../public/search.mp4";
+import ScrollAnimate from "../App/ScrollAnimate.vue";
 
 const { t } = useI18n();
+
+const videoElement = ref<HTMLVideoElement | null>(null);
+const duration = ref(0);
+
+onMounted(() => {
+  nextTick(() => {
+    setTimeout(() => {
+      if (videoElement.value) {
+        videoElement.value.onloadedmetadata = (): void => {
+          duration.value = videoElement.value?.duration ?? 0;
+        };
+      }
+    });
+  });
+});
 
 const router = useRouter();
 </script>
@@ -28,8 +45,22 @@ const router = useRouter();
       </div>
     </div>
 
-    <div class="mx-auto px-8 max-w-7xl">
-      <v-img class="border rounded-lg elevation-24" :src="system" />
-    </div>
+    <scroll-animate class="h-[800vh]">
+      <template #default="{ progress }">
+        <div class="h-main-full w-full flex flex-col justify-center items-center">
+          <div class="w-full max-w-7xl">
+            <video
+              ref="videoElement"
+              muted
+              class="w-full rounded-lg"
+              :currentTime="duration * progress"
+            >
+              duration: {{ duration }}
+              <source :src="system" type="video/mp4" />
+            </video>
+          </div>
+        </div>
+      </template>
+    </scroll-animate>
   </div>
 </template>
