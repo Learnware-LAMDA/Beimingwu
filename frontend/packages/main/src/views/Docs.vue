4067f4faa2734eb61aabfaed9b384d4ce3981b35
Qincheng Zheng <zhengqc@lamda.nju.edu.cn>
2023-11-09 19:42:07 +0800

    feat(frontend): change docs link

--- frontend/packages/main/src/views/Docs.vue
+++ frontend/packages/main/src/views/Docs.vue
@@ -1,10 +1,14 @@
 <script setup lang="ts">
 import { useRouter } from "vue-router";
+import { useI18n } from "vue-i18n";
 
 const router = useRouter();
+const { locale } = useI18n();
 
 router.go(-1);
-location.href = "http://36.111.128.21:30006/";
+location.href = `https://docs.beiming.cloud${
+  locale.value.toLocaleLowerCase() === "zh-cn" ? "/zh-CN/" : ""
+}`;
 </script>
 
 <template>
