a4ec165fa4be83ba672aa54aec3e38307235523a
zouxiaochuan <zouxiaochuan@163.com>
2023-11-12 21:07:48 +0800

    fix(backend): wait process end in test_learnware_client.py

--- backend/tests/test_learnware_client.py
+++ backend/tests/test_learnware_client.py
@@ -46,6 +46,7 @@ class TestLearnwareClient(unittest.TestCase):
     def tearDown(self) -> None:
         super().tearDown()
         self.server_process.kill()
+        self.server_process.join()
         testops.cleanup_folder()
         pass
 
