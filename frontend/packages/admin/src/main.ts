43902be547b78668c7027a9aa703df01d0bd523b
AnnyTerfect <1247470246@qq.com>
2023-11-23 23:38:12 +0800

    fix(frontend): driver css missing in admin

--- frontend/packages/admin/src/main.ts
+++ frontend/packages/admin/src/main.ts
@@ -6,5 +6,6 @@ import "@main/plugins/dayjs";
 import store from "@main/store";
 import i18n from "@main/i18n";
 import "./style.scss";
+import "driver.js/dist/driver.css";
 
 createApp(App).use(vuetify).use(router).use(store).use(i18n).mount("#app");
