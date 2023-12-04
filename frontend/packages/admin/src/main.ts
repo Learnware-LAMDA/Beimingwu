5322251181677a5bed1242989f74633658e9a54f
AnnyTerfect <1247470246@qq.com>
2023-12-04 20:03:10 +0800

    perf(frontend): admin reuse main style.scss

--- frontend/packages/admin/src/main.ts
+++ frontend/packages/admin/src/main.ts
@@ -5,7 +5,7 @@ import vuetify from "./plugins/vuetify";
 import "@main/plugins/dayjs";
 import store from "@main/store";
 import i18n from "@main/i18n";
-import "./style.scss";
+import "@main/style.scss";
 import "driver.js/dist/driver.css";
 
 createApp(App).use(vuetify).use(router).use(store).use(i18n).mount("#app");
