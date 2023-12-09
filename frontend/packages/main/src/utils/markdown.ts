a9bde28987d08909ad86d5f6dff75306eeffa2a5
AnnyTerfect <1247470246@qq.com>
2023-12-09 19:44:30 +0800

    feat(frontend): block markdown image

--- frontend/packages/main/src/utils/markdown.ts
+++ frontend/packages/main/src/utils/markdown.ts
@@ -3,6 +3,9 @@ import DOMPurify from "dompurify";
 
 const purify = DOMPurify(window);
 
+const renderer = new marked.Renderer();
+renderer.image = (): string => "";
+
 export default function html2Markdown(html: string): string {
-  return purify.sanitize(marked(html, { async: false }) as string);
+  return purify.sanitize(marked(html, { async: false, renderer }) as string);
 }
