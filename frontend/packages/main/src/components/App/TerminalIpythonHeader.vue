0ebcf49439d2e6184152177ba88c7dc8c92f15e3
AnnyTerfect <1247470246@qq.com>
2023-12-10 17:07:00 +0800

    perf(frontend): simplify ipython header

--- frontend/packages/main/src/components/App/TerminalIpythonHeader.vue
+++ frontend/packages/main/src/components/App/TerminalIpythonHeader.vue
@@ -3,12 +3,5 @@
     <b class="text-red-800">$&nbsp;</b>
     <span>ipython</span>
   </div>
-  <div>
-    <span>Python 3.9.16 (main, Apr 12 2021, 14:00:31)</span>
-    <br />
-    <span>[Clang 12.0.0 (clang-1200.0.32.29)] on darwin</span>
-    <br />
-    <span> Type "help", "copyright", "credits" or "license()" for more information. </span>
-    <br /><br />
-  </div>
+  <br />
 </template>
