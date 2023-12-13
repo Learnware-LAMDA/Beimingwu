3c5eef29d73f0ee92dc8adad787079ffbde7f14f
zouxiaochuan <zouxiaochuan@163.com>
2023-12-13 13:47:33 +0800

    feat(backend): remove stop words before search sensitive words

---
+++ backend/lib/sensitive_words_utils.py
@@ -0,0 +1,62 @@
+import re
+
+stopwords = set(
+    [
+        "!",
+        '"',
+        "#",
+        "$",
+        "%",
+        "&",
+        "'",
+        "(",
+        ")",
+        "*",
+        "+",
+        ",",
+        "-",
+        ".",
+        "/",
+        ":",
+        ";",
+        "<",
+        "=",
+        ">",
+        ">>",
+        "?",
+        "@",
+        "[",
+        "\\",
+        "]",
+        "^",
+        "}",
+        "~",
+        " ",
+        "。",
+        "、",
+    ]
+)
+
+stopwords_reg = "(" + "|".join([re.escape(s) for s in stopwords]) + ")"
+stopwords_pattern = re.compile(stopwords_reg)
+stopwords_between_hanzi_pattern = re.compile((r"([\u4e00-\u9fa5]{1})" + stopwords_reg + "+" + r"([\u4e00-\u9fa5]{1})"))
+
+
+def search_sensitive_words(text, sensitive_pattern):
+    """
+    Search sensitive words in text.
+    """
+
+    if sensitive_pattern is None:
+        return []
+
+    text = stopwords_pattern.sub(" ", text)
+    for i in range(3):
+        text = stopwords_between_hanzi_pattern.sub(r"\1\3", text)
+        pass
+    ret = []
+    for m in sensitive_pattern.finditer(text):
+        ret.append(m.group(0).strip())
+        pass
+
+    return ret
