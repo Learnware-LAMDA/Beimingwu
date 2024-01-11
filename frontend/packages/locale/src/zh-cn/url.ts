b9ccfee3003da6a36d0d521b3fdb789b90f883cf
GeneLiuXe <356340460@qq.com>
2024-01-11 16:42:40 +0800

    feat(frontend): add AboutUs url

--- frontend/packages/locale/src/zh-cn/url.ts
+++ frontend/packages/locale/src/zh-cn/url.ts
@@ -8,5 +8,6 @@ export default {
     HeteroSearchGuide: `${BASE_URL}user-guide/learnware-search.html#%E5%BC%82%E6%9E%84%E8%A1%A8%E6%A0%BC%E6%9F%A5%E6%90%9C`,
     UserAgreement: `${BASE_URL}user-agreement.html`,
     PrivacyPolicy: `${BASE_URL}privacy-policy.html`,
+    AboutUs: `${BASE_URL}about-us.html`,
   },
 };
